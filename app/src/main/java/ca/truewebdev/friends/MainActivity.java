86309bf7ef174c42af36fa930eea502066afafd7
True <trueking7@gmail.com>
2016-03-04 15:52:59 -0500

    preSearchActivity

--- app/src/main/java/ca/truewebdev/friends/MainActivity.java
+++ app/src/main/java/ca/truewebdev/friends/MainActivity.java
@@ -1,9 +1,12 @@
 package ca.truewebdev.friends;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if(fragmentManager.findFragmentById(android.R.id.content) == null){
+            FriendsListFragment friendsListFragment = new FriendsListFragment();
+            fragmentManager.beginTransaction().add(android.R.id.content, friendsListFragment).commit();
+
+
+        }
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -27,9 +37,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -37,12 +47,24 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()){
+            case R.id.addRecord:
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.deleteDatabase:
+                FriendsDialog dialog = new FriendsDialog();
+                Bundle args = new Bundle();
+                args.putString(FriendsDialog.DIALOG_TYPE, FriendsDialog.DELETE_DATABASE);
+                dialog.setArguments(args);
+                dialog.show(getSupportFragmentManager(), "delete-database");
+                break;
+            case R.id.searchRecords:
+                Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(searchIntent);
         }
+
+
 
         return super.onOptionsItemSelected(item);
     }
